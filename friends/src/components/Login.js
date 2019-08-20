dff736dc2beb04d90336260a8e2231cea225e410
Your Name <you@example.com>
2019-08-20 13:52:23 -0700

    created Login component

---
+++ friends/src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Login extends React.Component {
+    state = {
+
+    }
+
+
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Login;
