7b6c3b6c9b5648edacb639471710278becbae23e
Your Name <you@example.com>
2019-08-20 14:09:40 -0700

    done form and got a token from correct login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from './components/Login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's build a fun app!</h1>
+      <Login />
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,15 +1,58 @@
 import React from 'react';
+import axios from 'axios'
 
 
 class Login extends React.Component {
     state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
 
+    handleChange = e => {
+        console.log('e', e)
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:5000/api/login', this.state.credentials)
+            .then(res => 
+                console.log(res))
+            .catch(err => console.log(err.response))
     }
 
 
     render(){
+        console.log('credentials', this.state.credentials)
         return (
-            <div></div>
+            <div>
+                <form onSubmit = {this.handleSubmit}>
+                    <input 
+                    type="text" 
+                    name="username"
+                    placeholder="username"
+                    value={this.state.credentials.username} 
+                    onChange={this.handleChange}
+                     />
+                    
+                    <input 
+                    type="text" 
+                    placeholder="password" 
+                    name="password"
+                    value={this.state.credentials.password} 
+                    onChange={this.handleChange}
+                     />
+                    <button>Submit</button>
+                </form>
+            </div>
         )
     }
 }
