0d11f712be71d6d80fdfad46c5e54a51cca595b8
Your Name <you@example.com>
2019-08-20 15:24:12 -0700

    got a list of friends displayed

--- friends/src/components/Friendlist.js
+++ friends/src/components/Friendlist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 class Friendlist extends React.Component {
     state = {
@@ -6,15 +7,32 @@ class Friendlist extends React.Component {
     }
 
     componentDidMount(){
-
+        this.getData()
     }
 
     getData = () => {
-        
+        axiosWithAuth()
+        .get('http://localhost:5000/api/friends')
+        .then(res => 
+            // console.log('res in getData', res)
+            this.setState({
+                friendList: res.data
+            })
+            )
+        .catch(err => console.log(err.response))
     }
     render(){
+        console.log('friendList', this.state.friendList)
         return (
-            <div></div>
+            <div>
+                {this.state.friendList.map(friend => (
+                    <div>
+                        <h3>Name: {friend.name}</h3>
+                        <p>Email: {friend.email}</p>
+                        <p>Age: {friend.age}</p>
+                    </div>
+                ))}
+            </div>
         )
     }
 }
