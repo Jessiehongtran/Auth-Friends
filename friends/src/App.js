8cb968d3f7e8d29a35319321d6d3bd5d009cfc2f
Your Name <you@example.com>
2019-08-20 14:18:07 -0700

    added a route for login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Login from './components/Login'
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build a fun app!</h1>
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Let's build a fun app!</h1>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+      
+          <Route exact path="/login" component={Login} />
+      
+      
+      </div>
+    </Router>
   );
 }
 
